4bf74cf98188c126239e34e0d4cd94e0e340ca99
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-19 14:14:32 +0530

    3 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
+import React, { useState } from 'react';
+import { PaginationControl } from 'react-bootstrap-pagination-control';
 export default function Home() {
+  const [page, setPage] = useState(1);
   return (
     <div className={styles.container}>
       <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
-      <h1 className="underline text-red-100">Hello world</h1>
+      <div>
+        <h1
+          className={`underline text-red-600 ${
+            page == 1 ? 'visible' : 'invisible'
+          }`}
+        >
+          Hello world
+        </h1>
+      </div>
+      <PaginationControl
+        page={page}
+        between={4}
+        total={250}
+        limit={20}
+        changePage={(page) => {
+          setPage(page);
+        }}
+        ellipsis={1}
+      />
     </div>
   );
 }
